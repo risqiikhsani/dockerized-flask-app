4dafb0c29ef6fd4f795dc65f27ca1ec6305775e8
risqiikhsani <risqiikhsani16@gmail.com>
2024-12-30 13:46:34 +0700

    added logging

--- app.py
+++ app.py
@@ -10,14 +10,17 @@ DATABASE_URL = os.getenv("DATABASE_URL")
 
 @app.route("/")
 def home():
+    print("Home is accessed")
     return f"Connected to database: {DATABASE_URL}"
 
 @app.route("/health")
 def health():
+    print("Health is accessed")
     return "OK"
 
 @app.route("/version")
 def version():
+    print("Version is accessed")
     return "1.0.0"
 
 if __name__ == "__main__":
